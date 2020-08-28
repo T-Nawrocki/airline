623e3b7ffe1b0357240fe46293dd4b858e91eaea
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-28 13:50:57 +0100

    PlaneType enum added

--- src/main/java/plane/PlaneType.java
+++ src/main/java/plane/PlaneType.java
@@ -1,4 +1,17 @@
 package plane;
 
 public enum PlaneType {
+
+    AIRBUS_A320(150, 70000),
+    BOEING_737(175, 80000),
+    BOEING_777(400, 300000);
+
+    private int capacity;
+    private int maxWeight;
+
+    PlaneType(int capacity, int maxWeight) {
+        this.capacity = capacity;
+        this.maxWeight = maxWeight;
+    }
+
 }
