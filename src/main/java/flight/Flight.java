cec944dc773d76d79ca4287ced13a4929e583ff7
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-28 14:14:11 +0100

    flight passes initialisation tests

--- src/main/java/flight/Flight.java
+++ src/main/java/flight/Flight.java
@@ -1,4 +1,49 @@
 package flight;
 
+import passenger.Passenger;
+import plane.Plane;
+
+import java.util.ArrayList;
+
 public class Flight {
+
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private Airport destination;
+    private Airport departureAirport;
+    private String departureTime;
+
+    public Flight(Plane plane, String flightNumber, Airport destination, Airport departureAirport, String departureTime) {
+        this.passengers = new ArrayList<Passenger>();
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public Airport getDestination() {
+        return destination;
+    }
+
+    public Airport getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public int getNumberOfPassengers() {
+        return passengers.size();
+    }
 }
