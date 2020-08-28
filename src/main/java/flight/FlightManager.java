268f9bf34a8e80b3ebb00ebd2fe65415db62d18e
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-28 15:00:59 +0100

    FlightManager baggageAllocationPerPassenger added and passes test

--- src/main/java/flight/FlightManager.java
+++ src/main/java/flight/FlightManager.java
@@ -1,7 +1,14 @@
 package flight;
 
+import plane.Plane;
+
 public class FlightManager {
 
     private FlightManager() {};  // prevents class from being instantiated
+
+    public static int baggageAllocationPerPassenger(Flight flight) {
+        Plane plane = flight.getPlane();
+        return plane.maxBaggageWeight() / plane.getCapacity();  // truncates decimal places
+    }
 
 }
--- src/test/java/FlightManagerTest.java
+++ src/test/java/FlightManagerTest.java
@@ -1,2 +1,30 @@
+import flight.Airport;
+import flight.Flight;
+import org.junit.Before;
+import org.junit.Test;
+import passenger.Passenger;
+import plane.Plane;
+import plane.PlaneType;
+
+import static flight.FlightManager.baggageAllocationPerPassenger;
+import static org.junit.Assert.assertEquals;
+
 public class FlightManagerTest {
+
+    private Flight flight;
+    private Plane plane;
+    private Passenger passenger;
+
+    @Before
+    public void before() {
+        plane = new Plane(PlaneType.BOEING_737);
+        flight = new Flight(plane, "AB123", Airport.ARN, Airport.EDI, "16:30");
+        passenger = new Passenger("Jen", 3);
+    }
+
+    @Test
+    public void canGetBaggageAllocationPerPassenger() {
+        assertEquals(228, baggageAllocationPerPassenger(flight));
+    }
+
 }
